c0387cf7f519042918cf8722eb20d4d3c646a378
hqkhiem-member <shainalom8415@gmail.com>
2025-09-27 18:02:22 +0700

    change state

--- backend.tf
+++ backend.tf
@@ -1,9 +1,10 @@
 terraform {
   backend "s3" {
     bucket         = "699561668660-terraform-state"
-    key            = "terraform-state/test.tfstate"
+    key            = "terraform-state/test-2.tfstate"
     region         = "ap-southeast-1"
     encrypt        = true
     dynamodb_table = "699561668660-terraform-state"
   }
+
 }
